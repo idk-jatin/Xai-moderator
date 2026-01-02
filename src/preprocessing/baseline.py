87eadb0145c38f4055a59723ee6b3d00b77fd59a
idk-jatin <jatinkrpandit40@gmail.com>
2026-01-02 22:23:16 +0530

    Trained first baseline model

--- src/preprocessing/baseline.py
+++ src/preprocessing/baseline.py
@@ -3,16 +3,16 @@ from nltk.corpus import stopwords
 from emoji import demojize
 import pandas as pd
 
-Stop_Words = set(stopwords.words('english'))
+Stop_Words = set(stopwords.words("english")) - {"not", "no", "nor", "never"}
 
 
-def baseline_preprocessing(text: str)-> str:
+def baseline_preprocessing(text: str) -> str:
 
     text = text.lower()
 
     text = text.replace("<user>", " USER ")
     text = text.replace("<number>", " ")
-    
+
     text = re.sub(r"\b\d+\b", " ", text)
 
     text = demojize(text, delimiters=(" ", " "))
@@ -24,13 +24,14 @@ def baseline_preprocessing(text: str)-> str:
     return " ".join(tokens)
 
 
-def csv_preprocess(input,to_save):
+def csv_preprocess(input, to_save):
     df = pd.read_csv(input)
     df["text"] = df["text"].astype(str).apply(baseline_preprocessing)
-    bs_df = df[["post_id","source","text","label"]]
-    bs_df.to_csv(to_save,index=False)
+    bs_df = df[["post_id", "source", "text", "label"]]
+    bs_df.to_csv(to_save, index=False)
+
 
 if __name__ == "__main__":
-    csv_preprocess("data/raw/train.csv","data/baseline/train_base.csv")
-    csv_preprocess("data/raw/test.csv","data/baseline/test_base.csv")
-    csv_preprocess("data/raw/val.csv","data/baseline/val_base.csv")
+    csv_preprocess("data/raw/train.csv", "data/baseline/train_base.csv")
+    csv_preprocess("data/raw/test.csv", "data/baseline/test_base.csv")
+    csv_preprocess("data/raw/val.csv", "data/baseline/val_base.csv")
